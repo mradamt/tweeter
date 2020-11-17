f0f3c2e5a5873ca3fead7cefcf6a14873b257193
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-17 14:36:35 -0800

    Implement char-counter.js CONTINUED
    This version has functioning countdown for text area from 140, console.logged

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,38 +1,7 @@
 $(document).ready(function() {
-  const $tweetText = document.getElementById('tweet-text');
-  // const $tweetText = $('#tweet-text');
-  // const $tweetTextVal = $('#tweet-text').val();
+  document.getElementById('tweet-text').addEventListener('input', function() { 
+    // console.log(140 - this.value.length)
+    console.log(140 - $(this).val().length)
 
-
-  
-  // $tweetText.addEventListener('keydown', (event) => {
-  //   console.log(event.key)
-  //   console.log($('#tweet-text').val())
-  // })
-
-  // document.addEventListener('blur', (event) => { 
-  //   console.log('blur');
-  //   console.log(event);
-  // });
-  // document.addEventListener('click', (event) => { 
-  //   console.log('click');
-  //   console.log(event);
-  // });
-  // document.addEventListener('keydown', (event) => { 
-  //   console.log('keydown');
-  //   console.log(event);
-  // });
-  // document.addEventListener('keyup', (event) => { 
-  //   console.log('keyup');
-  //   console.log(event);
-  // });
-  $tweetText.addEventListener('change', (event) => { 
-    console.log('change');
-    console.log(event);
-  });
-  $tweetText.addEventListener('input', (event) => { 
-    console.log('input');
-    console.log(event);
-    console.log($('#tweet-text').val().length)
   });
 })
