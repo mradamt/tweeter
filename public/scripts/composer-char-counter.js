6d1d7bf30192e156605eb526b4500d97ca3db7a2
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-17 15:17:11 -0800

    Implement char-counter.js CONTINUED-2
    Status: correctly updates the counter with each key entered/deleted
    TODO: make the counter red when negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   document.getElementById('tweet-text').addEventListener('input', function() { 
-    // console.log(140 - this.value.length)
-    console.log(140 - $(this).val().length)
-
+    const counter = 140 - $(this).val().length
+    $(this).parent().find(".counter").val(counter)
   });
 })
