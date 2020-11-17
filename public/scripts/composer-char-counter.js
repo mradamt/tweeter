87dd65faa12bfe587d82972dcd25c4a1449fa908
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-17 15:38:30 -0800

    Implement char-counter.js (complete)
    - Add js function to live-update char counter below tweet
    - Change counter colour to red when negative
    - Use tree traversal for some reason instead of adding a css id to the counter html element

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,22 @@
+const { countReset } = require("console")
+
 $(document).ready(function() {
+
+  /* Event listener to track character count of text input, 
+   * update 'counter' immediately, and change colour of counter
+   * when negative */
   document.getElementById('tweet-text').addEventListener('input', function() { 
-    const counter = 140 - $(this).val().length
-    $(this).parent().find(".counter").val(counter)
+    // Traverse tree to find .counter class (output tag) and save to variable
+    const $counterEl = $(this).parent().find(".counter")
+
+    // Update counter value immediately with each char entered/deleted
+    $counterEl.val(140 - $(this).val().length);
+
+    // Change COLOR of counter element to red when negative
+    if ($counterEl.val() < 0) {
+      $('.counter').css("color", "red");
+    } else {
+      $('.counter').css("color", "inherit");
+    }
   });
 })
