5dc132605ef812e5fe47a3f473cf4ec8497a0572
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-18 13:36:11 -0800

    (Add comment to char-counter.js explaining current choice of method)
    TODO: review this method...

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
 
   /* Event listener to track character count of text input,
    * update 'counter' immediately, and change colour of counter
-   * when negative */
+   * when negative.
+   * Uses 'input' event not 'change' event because "Unlike the input event, the change 
+   * event is not necessarily fired for each alteration to an element's value."
+   * - MDN docs.../change_event  
+   */
   document.getElementById('tweet-text').addEventListener('input', function() {
     // Traverse tree to find .counter class (output tag) and save to variable
     const $counterEl = $(this).parent().find(".counter");
