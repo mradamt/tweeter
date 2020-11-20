a49ae766d55df5c3e9ac09e454abd8b437ce3161
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-19 19:42:13 -0800

    Lint /scripts

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -2,24 +2,24 @@ $(document).ready(function() {
 
   /* 'Zoom' effect on icon below 'write a tweet' in navbar */
   $('.navbar-right-side').hover(function() {
-    $('.navbar-right-side img').css('transform', 'scale(1.25)')
+    $('.navbar-right-side img').css('transform', 'scale(1.25)');
   }, function() {
-    $('.navbar-right-side img').css('transform', 'scale(1)')
-  })
+    $('.navbar-right-side img').css('transform', 'scale(1)');
+  });
   
-  /* Function to clean up styles added composer textarea on slideDown() 
+  /* Function to clean up styles added composer textarea on slideDown()
    * or slideToggle(), and scroll the window so composer is in view */
   const tidyUpComposer = () => {
-    $('#tweet-text').removeAttr('style').focus()
-    $(window).scrollTop(0)
-  }
+    $('#tweet-text').removeAttr('style').focus();
+    $(window).scrollTop(0);
+  };
 
   /* Toggle tweet composer section into/out of view on click
    * and focus curser into textarea ready to type */
   $('#navbar-right-side').click(function() {
-    $('.new-tweet').slideToggle('fast')
+    $('.new-tweet').slideToggle('fast');
     tidyUpComposer();
-  })
+  });
 
   /* Scroll-to-top button appears when window is scrolled down*/
   if ($(window).scroll(function() {
@@ -32,8 +32,8 @@ $(document).ready(function() {
 
   /* Scroll to top and ensure composer is OPEN when we get there */
   $('#scroll-to-top').click(function() {
-    $('.new-tweet').slideDown('fast')
-    tidyUpComposer()
-  })
+    $('.new-tweet').slideDown('fast');
+    tidyUpComposer();
+  });
   
-})
+});
